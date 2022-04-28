ec74db7db4180d48f2ae42720da1f6adc512f78a
michaelhb <mhurtadob@unsa.edu.pe>
2022-04-28 12:05:04 -0500

    Bienvenido al curso

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,5 +3,8 @@ public class HolaMundo
 	public static void main(String args[]) {
 		//HolaMundo
 		System.out.println("¡Hola Mundo!");
+
+		//Bienvenido al curso
+		System.out.println("Bienvenido al curso");
 	}
 }
